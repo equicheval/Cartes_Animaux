8a42a7e6ee30ca8d2357e33e05267e90921cb872
Nicolas Chesney <planet.wild@hotmail.fr>
2017-10-12 21:15:44 +0200

    Résolution du bug d'affichage des cartes
    - Ajout des Sorting Layers corrects
    - Mise à jour sur les Prefabs

---
+++ Assets/Scripts/Logic/CreatureScripts/Excès de rage/DamageAllFriendlyCreatures.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageAllFriendlyCreatures : CreatureEffect
+{
+	public DamageAllFriendlyCreatures(Player owner, CreatureLogic creature, int specialAmount, string specialName): base(owner, creature, specialAmount, specialName){}
+
+	// Excès de rage
+	public override void WhenACreatureIsPlayed()
+	{
+		if (TurnManager.Instance.whoseTurn.table.CreaturesOnTable.Count == 0)
+			return;
+
+		CreatureLogic[] CreaturesToDamage = TurnManager.Instance.whoseTurn.table.CreaturesOnTable.ToArray();
+		foreach (CreatureLogic cl in CreaturesToDamage)
+		{
+			new DealDamageCommand(cl.ID, specialAmount, healthAfter: cl.Health - specialAmount).AddToQueue();
+			cl.Health -= specialAmount;
+		}
+	}
+}
+
---
+++ Assets/Scripts/Logic/CreatureScripts/Excès de rage/DamageRandomCreature.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageRandomCreature : CreatureEffect
+{
+	public DamageRandomCreature(Player owner, CreatureLogic creature, int specialAmount, string specialName): base(owner, creature, specialAmount, specialName){}
+
+	// Excès de rage
+	public override void WhenACreatureIsPlayed()
+	{
+		if (TurnManager.Instance.whoseTurn.otherPlayer.table.CreaturesOnTable.Count == 0)
+			return;
+		
+		CreatureLogic[] CreatureToDamage = TurnManager.Instance.whoseTurn.otherPlayer.table.CreaturesOnTable.ToArray();
+		int index = Random.Range (0, CreatureToDamage.Length);
+
+		new DealDamageCommand(CreatureToDamage[index].ID, specialAmount, healthAfter: CreatureToDamage[index].Health - specialAmount).AddToQueue();
+		CreatureToDamage[index].Health -= specialAmount;
+	}
+}
+
